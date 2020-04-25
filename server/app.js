586b7e85118df036ee75cf368b27d3b29d3089a9
Serkan Akman <serkan.akman@boranka.com>
2020-04-26 01:26:26 +0300

    Son mongoDB ayarlamaları

--- server/app.js
+++ server/app.js
@@ -4,29 +4,78 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('./Calisan');
 
+app.use(bodyParser.json());
+
 const Calisan = mongoose.model('calisan');
 
 const PORT = 3333;
 const mongoURL = 'mongodb+srv://legend:z2LR2KRWEANtY1cB@empapp-iakgh.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(mongoURL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
     console.log("MongoDB bağlantı başarılı!");
 })
 
-mongoose.connection.on('error',(err)=>{
-    console.log("MongoDB bağlantı başarısız: ",err);
+mongoose.connection.on('error', (err) => {
+    console.log("MongoDB bağlantı başarısız: ", err);
 })
 
-app.get('/',(req,res) => {
-    res.send("Node js e hoşgeldin");
+app.get('/', (req, res) => {
+    Calisan.find({}).then(data => {
+        res.send(data)
+    }).catch(err => console.log("Hata : ",err));
 })
 
-app.listen(PORT,()=>{
+app.post('/send-data', (req, res) => {
+
+    const eleman = new Calisan({
+        name: req.body.name,
+        phone: req.body.phone,
+        url: req.body.url,
+        position: req.body.position,
+        email: req.body.email,
+        salary: req.body.salary
+    })
+
+    eleman.save().then(
+        data => {
+            console.log(data);
+            res.send(data);
+        }
+    ).catch(err => {
+        console.log("Hata : ", err);
+    })
+})
+
+app.post('/delete', (req, res) => {
+    Calisan.findByIdAndDelete(req.body.id)
+        .then(data => {
+            console.log("Silinen: ", data);
+            res.send(data)
+        })
+        .catch(err => { console.log("Hata: ", err) });
+})
+
+app.post('/update', (req, res) => {
+    Calisan.findByIdAndUpdate(req.body.id, {
+        name: req.body.name,
+        phone: req.body.phone,
+        url: req.body.url,
+        position: req.body.position,
+        email: req.body.email,
+        salary: req.body.salary,
+    }).then(data => {
+        console.log("Güncellenen: ", data);
+        res.send(data)
+    }).catch(err =>{
+        console.log("Hata : ",err)
+    })
+})
+
+app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda yayında`);
 })
-
